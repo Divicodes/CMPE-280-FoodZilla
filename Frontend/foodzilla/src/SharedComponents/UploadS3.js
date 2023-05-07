db99b4a80dae57d7bd0d75a9d5ab973f9a58392d
harshaanirudh <harsha.anirudh@gmail.com>
2023-05-06 18:17:18 -0700

    S3 bucket config

--- Frontend/foodzilla/src/SharedComponents/UploadS3.js
+++ Frontend/foodzilla/src/SharedComponents/UploadS3.js
@@ -1,28 +1,25 @@
-import S3FileUpload from 'react-s3';
+import S3FileUpload from "react-s3";
 
 export let S3config = {
-    bucketName: 'ubereats-harsha',
-    dirName: 'profilePics', /* optional */
-    region: 'us-west-1',
-    accessKeyId: 'AKIAVDV7U5IG5A5TDZS4',
-    secretAccessKey: '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',
-}
-
+  bucketName: "uberbucket98",
+  dirName: "media",
+  region: "us-east-2",
+  accessKeyId: "AKIAZO3UJQCC6GNNZNNG",
+  secretAccessKey: "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",
+};
 
 export let uploadProfilePics = (file) => {
-    
-    return S3FileUpload.uploadFile(file, S3config);
-        
-}
-export let uploadDishImages=(file)=>{
-    let dishConfig={}
-    Object.assign(dishConfig,S3config)
-    dishConfig.dirName="dishes/"
-    return S3FileUpload.uploadFile(file,dishConfig);
-}
-export let uploadRestImages=(file)=>{
-    let restConfig={}
-    Object.assign(restConfig,S3config)
-    restConfig.dirName="restImages/"
-    return S3FileUpload.uploadFile(file,restConfig);
-}
+  return S3FileUpload.uploadFile(file, S3config);
+};
+export let uploadDishImages = (file) => {
+  let dishConfig = {};
+  Object.assign(dishConfig, S3config);
+  dishConfig.dirName = "dishes/";
+  return S3FileUpload.uploadFile(file, dishConfig);
+};
+export let uploadRestImages = (file) => {
+  let restConfig = {};
+  Object.assign(restConfig, S3config);
+  restConfig.dirName = "restImages/";
+  return S3FileUpload.uploadFile(file, restConfig);
+};
